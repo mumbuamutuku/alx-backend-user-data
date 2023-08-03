8e0ae93ebe8f65d23e4f4174e7c74c9ef233594a
mumbuamutuku <mumbuamutuku@gmail.com>
2023-08-03 21:04:24 +0000

    task6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,7 +6,7 @@ Implement a hash_password function
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     """
     Generate a random salt and hash the password with the salt
     """
@@ -14,3 +14,10 @@ def hash_password(password):
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Hash the provided password with the same salt used for the hashed password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
