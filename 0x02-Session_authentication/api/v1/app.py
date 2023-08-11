a97aabf8d65260e8454480b7205665c06da253a8
mumbuamutuku <mumbuamutuku@gmail.com>
2023-08-11 11:06:45 +0000

    task 0

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv("AUTH_TYPE") == "auth":
+    auth = Auth()
+elif getenv("AUTH_TYPE") == "basic_auth":
+    auth = BasicAuth()
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """
+    Add a new error handler for this status code, the response must be:
+    a JSON: {"error": "Unauthorized"}
+    status code 401
+    you must use jsonify from Flask
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """
+    Add a new error handler for this status code, the response must be:
+    a JSON: {"error": "Forbidden"}
+    status code 403
+    you must use jsonify from Flask
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """
+    if auth is None, do nothing
+    if request.path is not part of this list ['/api/v1/status/',
+    '/api/v1/unauthorized/', '/api/v1/forbidden/'], do nothing
+    - you must use the method require_auth from the auth instance
+    if auth.authorization_header(request) returns None,
+    raise the error 401 - you must use abort
+    if auth.current_user(request) returns None,
+    raise the error 403 - you must use abort
+    """
+    authorizedList = ['/api/v1/status/',
+                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    if auth and auth.require_auth(request.path, authorizedList):
+        user = auth.current_user(request)
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
+        request.current_user = user
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""
+Auth py
+"""
+
+from flask import Flask, request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    a class to manage the API authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        returns True if the path is not in the list of strings excluded_paths:
+        Returns True if path is None
+        Returns True if excluded_paths is None or empty
+        Returns False if path is in excluded_paths
+        """
+        checkpath = path
+        if path is None or excluded_paths is None or not excluded_paths:
+            return True
+
+        if path[-1] == '/':
+            path = path[:-1]
+
+        contains_slash = False
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] == '/':
+                excluded_path = excluded_path[:-1]
+                contains_slash = True
+
+            if excluded_path.endswith('*'):
+                idx_after_last_slash = excluded_path.rfind('/') + 1
+                excluded = excluded_path[idx_after_last_slash:-1]
+
+                idx_after_last_slash = path.rfind('/') + 1
+                tmp_path = path[idx_after_last_slash:]
+
+                if excluded in tmp_path:
+                    return False
+
+            if contains_slash:
+                contains_slash = False
+
+        path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        If request is None,returns None
+        If request doesn’t contain the header key Authorization, returns None
+        Otherwise, return the value of the header request Authorization
+        """
+        if request is None:
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        returns None - request will be the Flask request object
+        """
+        request = Flask(__name__)
+        return None
